b044570c41d77e3fdf31be7981c4453cbe66d714
Clement Ojiguo <clementojigs@hotmail.com>
2024-01-22 15:47:26 +0100

    Initial Commit

---
+++ Crafty Furniture Ecommerce SwiftUI/Crafty_Furniture_Ecommerce_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crafty_Furniture_Ecommerce_SwiftUIApp.swift
+//  Crafty Furniture Ecommerce SwiftUI
+//
+//  Created by Clem OJ on 22/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Crafty_Furniture_Ecommerce_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
